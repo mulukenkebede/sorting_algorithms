e5181b64c7f6c6222f4e392d40c5b4bafa421090
mulukenkebede <106770288+mulukenkebede@users.noreply.github.com>
2022-11-05 21:57:37 +0300

    update 1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,31 +12,6 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, n, new_n;
 	int swap;
-/**
- * swap_num - Swap two integers in an array.
- * @x: The first integer to swap.
- * @y: The second integer to swap.
- */
-void swap_num(int *x, int *y)
-{
-	int temp;
-
-	temp = *x;
-	*x = *y;
-	*y = temp;
-}
-
-/**
- * bubble_sort - Sort an array of integers in ascending order.
- * @array: An array of integers to sort.
- * @size: The size of the array.
- *
- * Description: Prints the array after each swap.
- */
-void bubble_sort(int *array, size_t size)
-{
-	size_t i, len = size;
-	bool sorted = false;
 
 	if (array == NULL || size < 2)
 		return;
@@ -57,19 +32,5 @@ void bubble_sort(int *array, size_t size)
 			}
 		}
 		n = new_n;
-	while (sorted == false)
-	{
-		sorted = true;
-		for (i = 0; i < len - 1; i++)
-		{
-			if (array[i] > array[i + 1])
-			{
-				swap_num(array + i, array + i + 1);
-				print_array(array, size);
-				sorted = false;
-			}
-		}
-		len--;
->>>>>>> c8f665d34abc1629cf761db1b2f3d83929f244dd
 	}
 }
