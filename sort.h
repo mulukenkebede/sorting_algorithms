27fb721b33e09adbcdaf7779ea5a7c6ff56a8ee7
mulukenkebede <106770288+mulukenkebede@users.noreply.github.com>
2022-11-05 21:29:45 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,6 +1,24 @@
-#ifndef __SORT__
-#define __SORT__
+#ifndef SORT_H
+#define SORT_H
+
+/*includes*/
+#include <stdio.h>
 #include <stdlib.h>
+
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 /**
  * struct listint_s - Doubly linked list node
@@ -16,14 +34,22 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* MACROS */
-#define FALSE 0
-#define TRUE 1
+/*Given printing functions*/
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
 
-/* TYPES */
-typedef unsigned char BOOL;
-
-/* SORT FUNCTIONS */
+/*Sorting algorithms*/
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
